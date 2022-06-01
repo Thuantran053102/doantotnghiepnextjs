297fed71ed269936ad92aaa9f31fa807a75ca63e
Thuantran53102 <tranthuan12a3@gmail.com>
2022-06-01 10:00:37 +0700

    upfirst

---
+++ pages/view/News/index.tsx
@@ -0,0 +1,13 @@
+import Styles from "./News.module.scss"
+import c from "clsx"
+import React from "react"
+import Head from 'next/head'
+export default function News(){
+    return(
+        <>
+            <Head>
+                <title>product</title>
+            </Head>
+        </>
+    )
+}
